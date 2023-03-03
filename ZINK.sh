ecd31a4841f6bf5ef05add8e3e551ec9b3fe005e
leem919 <leem919@gmail.com>
2023-03-02 18:06:57 -0800

    Update ZINK.sh

--- ZINK.sh
+++ ZINK.sh
@@ -10,5 +10,7 @@ cp /usr/include/libdrm/drm.h /usr/include/libdrm/drm_mode.h /usr/include
 cd mesa
 meson build -D platforms=x11,wayland -D gallium-drivers=swrast,virgl,zink -D vulkan-drivers=freedreno -D dri3=enabled  -D egl=enabled  -D gles2=enabled -D glvnd=true -D glx=dri  -D libunwind=disabled -D osmesa=true  -D shared-glapi=enabled -D microsoft-clc=disabled  -D valgrind=disabled --prefix /usr -D gles1=enabled -D freedreno-kgsl=true
 ninja -C build install
+cd ~
 cd ~/Zink-Mesa-Xlib
 meson . build -Dgallium-va=false -Ddri-drivers= -Dgallium-drivers=virgl,zink,swrast -Ddri3=true -Dvulkan-drivers= -Dglx=xlib -Dplatforms=x11 -Dbuildtype=release && ninja -C build install
+cd ~
