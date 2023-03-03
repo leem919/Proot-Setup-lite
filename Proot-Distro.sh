29724c4a183b0d4c5eba03d5d19da50ecaca43ce
leem919 <leem919@gmail.com>
2023-03-03 00:30:51 -0800

    Added script to launch wine desktop

--- Proot-Distro.sh
+++ Proot-Distro.sh
@@ -14,8 +14,11 @@ echo 'vncserver -name remote-desktop -geometry 960x540 -localhost no :1' > /usr/
 
 echo 'vncserver -kill :1' > /usr/local/bin/vnc-stop
 
+echo 'box64 wine64 explorer /desktop=wine,960x540 explorer' > /usr/local/bin/wine-desktop
+
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
+chmod +x /usr/local/bin/wine-desktop
 
 echo "export DISPLAY=:1" >> /etc/profile
 echo "export PULSE_SERVER=127.0.0.1" >> /etc/profile
