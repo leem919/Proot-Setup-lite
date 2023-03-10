42d3cda7b6f3d8ca9d893dc723b05077729d8ecd
leem919 <leem919@gmail.com>
2023-03-10 11:12:18 -0800

    Update virgl-testing.sh

--- virgl-testing.sh
+++ virgl-testing.sh
@@ -1,7 +1,21 @@
-pkg install x11-repo 
+pkg install x11-repo clang lld binutils cmake autoconf automake libtool ndk-sysroot ndk-multilib make python git libandroid-shmem-static 'vulkan*' ninja llvm bison flex libx11 xorgproto libdrm libpixman libxfixes libjpeg-turbo xtrans libxxf86vm xorg-xrandr xorg-font-util xorg-util-macros libxfont2 libxkbfile libpciaccess xcb-util-renderutil xcb-util-image xcb-util-keysyms xcb-util-wm xorg-xkbcomp xkeyboard-config libxdamage libxinerama
 curl https://raw.githubusercontent.com/leem919/Proot-Setup-lite/main/virglrenderer-android_0.10.4.deb -O
 dpkg -i virglrenderer-android_0.10.4.deb
 rm virglrenderer-android_0.10.4.deb
+LD_PRELOAD='' git clone --depth 1 -b mesa-22.0.5 https://gitlab.freedesktop.org/mesa/mesa.git
+cd mesa
+sed -i '40s+^$+#include "X11/Xlib.h"+' src/egl/main/egldisplay.h;
+sed -i 's/^import os$/import os, shutil\ndef link(src, dest):\n shutil.copyfile(src, dest)\ndef unlink(src):\n os.remove(src)\nos.link = link\nos.unlink = unlink/' bin/install_megadrivers.py;
+mkdir build
+cd build
+LDFLAGS='-l:libandroid-shmem.a -llog' meson .. -Dprefix=$PREFIX -Dplatforms=x11 -Dgbm=enabled -Ddri-drivers='' -Dgallium-drivers=zink,swrast -Dllvm=disabled -Dvulkan-drivers='' -Dcpp_rtti=false -Dc_args=-Wno-error=incompatible-function-pointer-types -Dbuildtype=release
+rm $PREFIX/lib/libglapi.so*
+rm $PREFIX/lib/libGL.so*
+rm $PREFIX/lib/libGLES*
+rm $PREFIX/lib/libEGL*
+rm $PREFIX/lib/libgbm*
+ninja install
+
 
 echo 'alias gl="virgl_test_server_android"' >> /data/data/com.termux/files/home/.bashrc
 echo 'alias gl="GALLIUM_DRIVER=virpipe"' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
