caa30486c93b22644492b00634ed449d53d0b4fd
leem919 <leem919@gmail.com>
2023-03-16 13:53:49 -0700

    Update Install_Script.sh

--- Install_Script.sh
+++ Install_Script.sh
@@ -7,14 +7,14 @@ echo "hide-soft-keyboard-on-startup = true" >> ~/.termux/termux.properties
 termux-setup-storage
 pkg clean && yes | pkg update &&
 pkg install nano wget proot-distro pulseaudio -y && pkg clean &&
-proot-distro install debian &&
+proot-distro install ubuntu &&
 proot-distro clear-cache &&
 
-echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> ~/.login-debian &&
-echo 'proot-distro login debian --shared-tmp --no-sysvipc' >> ~/.login-debian &&
-echo 'alias debian="source ~/.login-debian"' >> ~/.bashrc
+echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> ~/.login-ubuntu &&
+echo 'proot-distro login ubuntu --shared-tmp --no-sysvipc' >> ~/.login-ubuntu &&
+echo 'alias ubuntu="source ~/.login-ubuntu"' >> ~/.bashrc
 
-source ~/.login-debian
+source ~/.login-ubuntu
 
 # Install proot 
 
@@ -25,4 +25,4 @@ curl https://raw.githubusercontent.com/leem919/Proot-Setup-lite/main/Proot-Distr
 curl https://raw.githubusercontent.com/leem919/Proot-Setup-lite/main/Box86-64_Wine86-64.sh | bash
 
 echo "DONE"
-echo "Restart Termux, and then run debian to login to the proot. In the proot, run vnc-start to open the vnc server, and vnc-stop to close it. Once logged into vnc, run wine-desktop to start it up. If it fails with a kernel32.dll error, delete ~/.wine and try running gst before wine-desktop."
+echo "Restart Termux, and then run ubuntu to login to the proot. In the proot, run vnc-start to open the vnc server, and vnc-stop to close it. Once logged into vnc, run wine-desktop to start it up. If it fails with a kernel32.dll error, delete ~/.wine and try running gst before wine-desktop."
