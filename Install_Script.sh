145e0606d0c420264bcd70ee342668258de0cafe
leem919 <leem919@gmail.com>
2023-03-15 20:20:16 -0700

    Update Install_Script.sh

--- Install_Script.sh
+++ Install_Script.sh
@@ -9,8 +9,11 @@ pkg clean && yes | pkg update &&
 pkg install nano wget proot-distro pulseaudio -y && pkg clean &&
 proot-distro install ubuntu &&
 proot-distro clear-cache &&
-echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> ~/.login-ubuntu &&        
+
+echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> ~/.login-ubuntu &&
 echo 'proot-distro login ubuntu --shared-tmp --no-sysvipc' >> ~/.login-ubuntu &&
+echo 'alias ubuntu="source ~/.login-ubuntu"' >> ~/.bashrc
+
 source ~/.login-ubuntu
 
 # Install proot 
