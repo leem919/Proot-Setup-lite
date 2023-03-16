fd1ae5b14265319f3578c2d9197fd362eabd9b84
leem919 <leem919@gmail.com>
2023-03-15 19:19:07 -0700

    Rebuilt wine and changed proot

--- Install_Script.sh
+++ Install_Script.sh
@@ -9,9 +9,13 @@ pkg clean && yes | pkg update &&
 pkg install nano wget proot-distro pulseaudio -y && pkg clean &&
 proot-distro install ubuntu &&
 proot-distro clear-cache &&
-echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> ~/.bashrc &&        
-echo 'proot-distro login ubuntu --shared-tmp --no-sysvipc' >> ~/.bashrc &&
-source ~/.bashrc
+echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> ~/.login-ubuntu &&        
+echo 'proot-distro login ubuntu --shared-tmp --no-sysvipc' >> ~/.login-ubuntu &&
+echo 'alias ubuntu="source ~/.login-ubuntu"' >> ~/.bashrc &&
+echo 'source ~/.login-ubuntu' >> ~/.shortcuts/wine-desktop &&
+echo 'source /usr/local/bin/vnc-start' >> ~/.shortcuts/wine-desktop &&
+echo 'source /usr/local/bin/wine-desktop' >> ~/.shortcuts/wine-desktop &&
+source ~/.login-ubuntu
 
 # Install proot 
 
@@ -21,4 +25,4 @@ curl https://raw.githubusercontent.com/leem919/Proot-Setup-lite/main/Proot-Distr
 
 curl https://raw.githubusercontent.com/leem919/Proot-Setup-lite/main/Box86-64_Wine86-64.sh | bash
 
-echo "Restart Termux, and then run vnc-start to open the vnc server, and vnc-stop to close it. Once logged into vnc, run wine-desktop to start it up. If it fails, delete ~/.wine and try running gst wine-desktop instead."
+echo "Restart Termux, and then run ubuntu to login to the proot. In the proot, run vnc-start to open the vnc server, and vnc-stop to close it. Once logged into vnc, run wine-desktop to start it up. If it fails, delete ~/.wine and try running gst wine-desktop instead."
