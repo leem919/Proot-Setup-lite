6b2a483066ff966c23edb23edc919ef6fdb4f23f
leem919 <leem919@gmail.com>
2023-03-17 12:01:11 -0700

    Readded vnc server and wine shortcut

--- Proot-Distro.sh
+++ Proot-Distro.sh
@@ -17,10 +17,13 @@ echo 'vncserver -kill :1' > /usr/local/bin/vnc-stop
 
 echo 'box64 wine64 explorer /desktop=wine,1024x576 explorer' > /usr/local/bin/wine-desktop
 
+echo 'source /usr/local/bin/vnc-start; source /usr/local/bin/wine-desktop' > /usr/local/bin/wine-vnc
+
 chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
 chmod +x /usr/local/bin/wine-desktop
+chmod +x /usr/local/bin/wine-vnc
 
 echo "export DISPLAY=:1" >> /etc/profile
 echo "export PULSE_SERVER=127.0.0.1" >> /etc/profile
