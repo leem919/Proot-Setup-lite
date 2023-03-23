64b53af6c094a8402316a485ea568c35b4587179
leem919 <leem919@gmail.com>
2023-03-22 21:22:59 -0700

    Update zink-proot-testing.sh

--- zink-proot-testing.sh
+++ zink-proot-testing.sh
@@ -14,7 +14,7 @@ git apply --reject --whitespace=fix mesa20221011.patch
 wiggle --replace src/egl/main/egldisplay.h src/egl/main/egldisplay.h.rej
 mkdir build
 cd build
-meson .. -Dprefix=/usr/local -Dplatforms=x11 -Dgbm=enabled -Ddri-drivers='' -Dgallium-drivers=zink,swrast -Dllvm=enabled -Dvulkan-drivers='' -Dcpp_rtti=false -Dc_args=-Wno-error=incompatible-pointer-types -Dbuildtype=release
+meson .. -Dprefix=/usr/local -Dplatforms=x11 -Dgbm=enabled -Ddri-drivers='' -Dgallium-drivers=zink,swrast -Dllvm=disabled -Dvulkan-drivers='' -Dcpp_rtti=false -Dc_args=-Wno-error=incompatible-pointer-types -Dbuildtype=release
 rm -rf $PREFIX/lib/libglapi.so* $PREFIX/lib/libGL.so*
 ninja install
 echo 'alias zink="GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy"' >> ~/.bashrc
