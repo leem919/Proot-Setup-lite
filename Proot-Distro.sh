c04aa2159613efb62165075432e92d42f41f8c0b
leem919 <leem919@gmail.com>
2023-03-26 17:59:38 -0700

    Update Proot-Distro.sh

--- Proot-Distro.sh
+++ Proot-Distro.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 apt update && apt upgrade -y
-apt-get install sudo nano wget xterm dbus-x11 -y
+apt-get install sudo nano wget xterm dbus-x11 libxdamage1 libxfont2 x11-xkb-utils -y
 wget https://versaweb.dl.sourceforge.net/project/tigervnc/stable/1.13.1/ubuntu-22.04LTS/arm64/tigervncserver_1.13.1-1ubuntu1_arm64.deb
 dpkg -i tigervncserver_1.13.1-1ubuntu1_arm64.deb
 rm tigervncserver_1.13.1-1ubuntu1_arm64.deb
-apt --fix-broken install -y
 
 apt clean && apt autoremove -y
 
