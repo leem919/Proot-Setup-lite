27ea767527e2ef2f3e3100cf60276765ed5f16ed
leem919 <leem919@gmail.com>
2023-04-01 15:15:45 -0700

    Update virgl.sh

--- virgl.sh
+++ virgl.sh
@@ -1,5 +1,5 @@
 pkg install x11-repo -y
 pkg install virglrenderer-android -y
 
-echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android"' >> /data/data/com.termux/files/home/.bashrc
+echo 'MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &' >> /data/data/com.termux/files/home/.bashrc
 echo 'export MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=3.3COMPAT GALLIUM_DRIVER=virpipe WINEDEBUG=-all' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
