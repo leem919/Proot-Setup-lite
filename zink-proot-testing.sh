0e63b90d474c360389ee378a8e594e855e5dcbfa
leem919 <leem919@gmail.com>
2023-04-08 21:24:02 -0700

    Update zink-proot-testing.sh

--- zink-proot-testing.sh
+++ zink-proot-testing.sh
@@ -7,15 +7,15 @@ apt build-dep mesa -y
 cp /usr/include/libdrm/* /usr/include
 cd
 git clone https://github.com/Heasterian/mesa
-wget https://github.com/alexvorxx/Zink-Mesa-Xlib/archive/refs/tags/v0.0.1-alpha.zip
-unzip v0.0.1-alpha.zip
-rm v0.0.1-alpha.zip
+wget https://github.com/alexvorxx/Zink-Mesa-Xlib/archive/refs/tags/v0.0.2.zip
+unzip v0.0.2.zip
+rm v0.0.2.zip
 cd mesa
 meson build -D platforms=x11,wayland -D gallium-drivers=swrast,virgl,zink -D vulkan-drivers=freedreno -D dri3=enabled -D egl=enabled -D gles2=enabled -D glvnd=true -D glx=dri -D libunwind=disabled -D osmesa=true -D shared-glapi=enabled -D microsoft-clc=disabled -D valgrind=disabled --prefix /usr -D gles1=disabled -D freedreno-kgsl=true -Dbuildtype=release
 cd build
 ninja install
 cd
-cd Zink-Mesa-Xlib-0.0.1-alpha
+cd Zink-Mesa-Xlib-0.0.2
 meson build -Dgallium-va=disabled -Dgallium-drivers=virgl,zink,swrast -Ddri3=disabled -Dvulkan-drivers= -Dglx=xlib -Dplatforms=x11 -Dbuildtype=release
 cd build
 ninja install
