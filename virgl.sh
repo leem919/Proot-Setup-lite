7d1373248a139753730773f7fd0114dbe8c362f8
leem919 <leem919@gmail.com>
2023-04-11 08:58:22 -0700

    Update virgl.sh

--- virgl.sh
+++ virgl.sh
@@ -3,5 +3,5 @@ pkg install virglrenderer-android -y
 
 echo 'virgl_test_server_android &' >> /data/data/com.termux/files/home/.bashrc
 echo 'export GALLIUM_DRIVER=virpipe' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
-echo 'alias gl4="MESA_GL_VERSION_OVERRIDE=4.0"' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
-echo "Restart Termux to apply the changes, and use 'gl4' before a command or program to use MESA_GL_VERSION_OVERRIDE 4.0."
+echo 'alias gl4="MESA_GL_VERSION_OVERRIDE=4.3COMPAT"' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
+echo "Restart Termux to apply the changes, and use 'gl4' before a command or program to use MESA_GL_VERSION_OVERRIDE 4.3COMPAT."
